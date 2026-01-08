a4689a6ec047d73327d977d7d0cb93c7f77d532a
ayyappanmohan <madeshmohan@Madeshs-MacBook-Air.local>
2026-01-08 15:08:26 +0530

    var

--- p1.py
+++ p1.py
@@ -1,13 +1,6 @@
-class Login:
-    def __init__(self):
-        self.users = {"admin": "1234"}
-
-    def authenticate(self, username, password):
-        if username in self.users and self.users[username] == password:
-            print("Login Successful")
-        else:
-            print("Invalid Credentials")
-
+from login import Login
 
 login = Login()
 login.authenticate("admin", "1234")
+
+print("Welcome to Library Management System")
